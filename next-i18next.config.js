9e7705e40aca2ee893236d14d58cc2e0b5057b3d
malek <malek.2006ar@gmail.com>
2025-10-17 00:44:42 +0300

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'ar',
     locales: ['ar', 'en', 'fr', 'es', 'zh', 'hi', 'ru'],
     localeDetection: false,
   },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
